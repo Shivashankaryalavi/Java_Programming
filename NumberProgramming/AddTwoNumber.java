f7c128509b54928a3052a6b9bae88fbf9b95fee6
Shivashankaryalavi <shivashankaryalavi@gmail.com>
2026-03-30 20:17:11 +0530

    Add java program to find addition of two number

---
+++ NumberProgramming/AddTwoNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class AddTwoNumber {
+
+    public static int add(int a, int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number :");
+        int a =sc.nextInt();
+        System.out.println("Enter second number");
+        int b = sc.nextInt();
+        int result = add(a, b);
+        System.out.println("Addition of Two number is : "+ result);
+    }
+}
