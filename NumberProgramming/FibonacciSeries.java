f299816dbfadd0914b22ef7eb8c1abac7a4a5511
Shivashankaryalavi <shivashankaryalavi@gmail.com>
2026-04-04 22:18:48 +0530

    Add java program to print a fibonacci series

--- NumberProgramming/FibonacciSeries.java
+++ NumberProgramming/FibonacciSeries.java
@@ -4,5 +4,23 @@
 package NumberProgramming;
 import java.util.Scanner;
 public class FibonacciSeries {
+    public static void fib(int n){
+        int a =0;
+        int b =1;
+        for(int i =0;i<n;i++){
+            System.out.print(a+" ");
+            int next = a+b;
+            a=b;
+            b=next;
+         
+        }
+     
+    }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a number");
+    fib(sc.nextInt());
+    sc.close();
     
+  }
 }
