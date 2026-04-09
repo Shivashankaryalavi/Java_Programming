0c39b659c7583460f261af914ffe6c78724f6305
Shivashankaryalavi <shivashankaryalavi@gmail.com>
2026-04-09 16:02:25 +0530

    Add java program to find sum of digit

---
+++ NumberProgramming/SumOfDigits.java
@@ -0,0 +1,25 @@
+package NumberProgramming;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+
+    static int sum(int num){
+        int sum =0;
+        while(num>0){
+            int ld = num%10;
+            sum+=ld;
+            num/=10;
+
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int num = sc.nextInt();
+        System.out.println("Sum of Digit is "+ sum(num));
+        sc.close();
+    }
+    
+}
