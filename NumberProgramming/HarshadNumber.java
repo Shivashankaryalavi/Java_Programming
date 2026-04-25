119e5fc7a770c06aff94b869619744849554abe2
Shivashankaryalavi <shivashankaryalavi@gmail.com>
2026-04-25 18:40:53 +0530

    Added Harshad number

---
+++ NumberProgramming/HarshadNumber.java
@@ -0,0 +1,32 @@
+package NumberProgramming;
+import java.util.Scanner;
+
+public class HarshadNumber {
+
+    public static boolean isHarshad(int num) {
+        int sum = 0;
+        int temp = num;
+
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+
+        return num % sum == 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (isHarshad(num)) {
+            System.out.println(num + " is a Harshad Number");
+        } else {
+            System.out.println(num + " is NOT a Harshad Number");
+        }
+
+        sc.close();
+    }
+}
